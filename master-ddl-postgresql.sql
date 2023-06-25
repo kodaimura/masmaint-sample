857bb8b353b126e2807fbfcf2410eb5d5aae4d53
kodaimura <kodaimura123@gmail.com>
2023-06-25 20:35:51 +0900

    add ddl

---
+++ master-ddl-postgresql.sql
@@ -0,0 +1,33 @@
+create function set_update_time() returns trigger AS '
+    BEGIN
+      new.update_at := ''now'';
+      return new;
+    END;
+' language 'plpgsql';
+
+CREATE TABLE employees (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  address TEXT,
+  hire_date DATE,
+  job_title TEXT,
+  department_id INTEGER,
+  salary INTEGER
+);
+
+CREATE TABLE departments (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL UNIQUE,
+  description TEXT,
+  manager_id INTEGER,
+  location TEXT,
+  budget NUMERIC(10, 2),
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+create trigger trg_departments_upd AFTER UPDATE ON departments FOR EACH ROW
+  execute procedure set_update_time();

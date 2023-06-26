30539a0616604d5b8016be1749342663886167f7
kodaimura <kodaimura123@gmail.com>
2023-06-26 23:53:19 +0900

    chage action: change bg-color

--- golang/masmaint/web/static/js/department.js
+++ golang/masmaint/web/static/js/department.js
@@ -1,21 +1,45 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 	fetch('api/department')
 	.then(response => response.json())
-	.then(data  => renderTbody(data));
+	.then(data  => setData(data));
 });
+
+
+const setData = async (data) => {
+	await renderTbody(data);
+	addChangedAction('name');
+	addChangedAction('description');
+	addChangedAction('manager_id');
+	addChangedAction('location');
+	addChangedAction('budget');
+}
 
 const renderTbody = (data) => {
 	let tbody= '';
 	for (const elem of data) {
 		tbody += `<tr><td>${elem.id}</td>`
-		+ `<td>${elem.name}</td>`
-		+ `<td>${elem.description}</td>`
-		+ `<td>${elem.manager_id}</td>`
-		+ `<td>${elem.location}</td>`
-		+ `<td>${elem.budget}</td>`
+		+ `<td><input type="text" name="name" value=${elem.name}><input type="hidden" name="name_bk" value=${elem.name}></td>`
+		+ `<td><input type="text" name="description" value=${elem.description}><input type="hidden" name="description_bk" value=${elem.description}></td>`
+		+ `<td><input type="text" name="manager_id" value=${elem.manager_id}><input type="hidden" name="manager_id_bk" value=${elem.manager_id}></td>`
+		+ `<td><input type="text" name="location" value=${elem.location}><input type="hidden" name="location_bk" value=${elem.location}></td>`
+		+ `<td><input type="text" name="budget" value=${elem.budget}><input type="hidden" name="budget_bk" value=${elem.budget}></td>`
 		+ `<td>${elem.created_at}</td>`
 		+ `<td>${elem.updated_at}</td></tr>`
 	}
 
 	document.getElementById('list-body').innerHTML = tbody;
 }
+
+const addChangedAction = (columnName) => {
+	const elems = document.getElementsByName(columnName);
+	const elems_bk = document.getElementsByName(`${columnName}_bk`);
+	for (let i = 0; i < elems.length; i++) {
+		elems[i].addEventListener('change', () => {
+			if (elems[i].value !== elems_bk[i].value) {
+				elems[i].classList.add('changed');
+			} else {
+				elems[i].classList.remove('changed');
+			}
+		});
+	}
+} 
--- golang/masmaint/web/static/js/employee.js
+++ golang/masmaint/web/static/js/employee.js
@@ -1,23 +1,53 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 	fetch('api/employee')
 	.then(response => response.json())
-	.then(data  => renderTbody(data));
+	.then(data  => setData(data));
 });
+
+
+const setData = async (data) => {
+	await renderTbody(data);
+	addChangedAction('first_name');
+	addChangedAction('last_name');
+	addChangedAction('email');
+	addChangedAction('phone_number');
+	addChangedAction('address');
+	addChangedAction('hire_date');
+	addChangedAction('job_title');
+	addChangedAction('department');
+	addChangedAction('salary');
+}
 
 const renderTbody = (data) => {
 	let tbody= '';
 	for (const elem of data) {
 		tbody += `<tr><td>${elem.id}</td>`
-		+ `<td>${elem.first_name}</td>`
-		+ `<td>${elem.last_name}</td>`
-		+ `<td>${elem.email}</td>`
-		+ `<td>${elem.phone_number}</td>`
-		+ `<td>${elem.address}</td>`
-		+ `<td>${elem.hire_date}</td>`
-		+ `<td>${elem.job_title}</td>`
-		+ `<td>${elem.department_id}</td>`
-		+ `<td>${elem.salary}</td></tr>`
+		+ `<td><input type="text" name="first_name" value=${elem.first_name}><input type="hidden" name="first_name_bk" value=${elem.first_name}></td>`
+		+ `<td><input type="text" name="last_name" value=${elem.last_name}><input type="hidden" name="last_name_bk" value=${elem.last_name}></td>`
+		+ `<td><input type="text" name="email" value=${elem.email}><input type="hidden" name="email_bk" value=${elem.email}></td>`
+		+ `<td><input type="text" name="phone_number" value=${elem.phone_number}><input type="hidden" name="phone_number_bk" value=${elem.phone_number}></td>`
+		+ `<td><input type="text" name="address" value=${elem.address}><input type="hidden" name="address_bk" value=${elem.address}></td>`
+		+ `<td><input type="text" name="hire_date" value=${elem.hire_date}><input type="hidden" name="hire_date_bk" value=${elem.hire_date}></td>`
+		+ `<td><input type="text" name="job_title" value=${elem.job_title}><input type="hidden" name="job_title_bk" value=${elem.job_title}></td>`
+		+ `<td><input type="text" name="department_id" value=${elem.department_id}><input type="hidden" name="department_id_bk" value=${elem.department_id}></td>`
+		+ `<td><input type="text" name="salary" value=${elem.salary}><input type="hidden" name="salary_bk" value=${elem.salary}></td></tr>`
 	}
 
 	document.getElementById('list-body').innerHTML = tbody;
 }
+
+const addChangedAction = (columnName) => {
+	const elems = document.getElementsByName(columnName);
+	const elems_bk = document.getElementsByName(`${columnName}_bk`);
+	for (let i = 0; i < elems.length; i++) {
+		elems[i].addEventListener('change', () => {
+			if (elems[i].value !== elems_bk[i].value) {
+				elems[i].classList.add('changed');
+			} else {
+				elems[i].classList.remove('changed');
+			}
+		});
+	}
+} 
+
+

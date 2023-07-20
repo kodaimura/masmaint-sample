e02d3cf4e514e41ae994a3dd7327d8e9e462874b
kodaimura <kodaimura123@gmail.com>
2023-07-20 16:56:33 +0900

    support mysql < 8.0.21

--- php/masmaint/app/dependencies.php
+++ php/masmaint/app/dependencies.php
@@ -46,10 +46,10 @@ return function (ContainerBuilder $containerBuilder) {
             $password = $dbSettings['password'];
             $charset = $dbSettings['charset'];
             $flags = $dbSettings['flags'];
-            //$dsn = "$driver:host=$host;port=$port;dbname=$dbname;charset=$charset"; //mysql
+            $dsn = "$driver:host=$host;port=$port;dbname=$dbname;charset=$charset"; //mysql
             //$dsn = "$driver:host=$host;port=$port;dbname=$dbname"; //postgresql
-            return new PDO("sqlite:../../../masmaint-sample.db"); //sqlite3
-            //return new PDO($dsn, $username, $password, $flags); //mysql postgresql
+            //return new PDO("sqlite:../../../masmaint-sample.db"); //sqlite3
+            return new PDO($dsn, $username, $password, $flags); //mysql postgresql
             
         },
     ]);
--- php/masmaint/app/repositories.php
+++ php/masmaint/app/repositories.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 use App\Application\Models\Daos\DepartmentDao;
 use App\Application\Models\Daos\EmployeeDao;
-use App\Application\Models\DaoImpls\DepartmentDaoImpl;
-use App\Application\Models\DaoImpls\EmployeeDaoImpl;
+use App\Application\Models\DaoImpls\DepartmentDaoImpl2;
+use App\Application\Models\DaoImpls\EmployeeDaoImpl2;
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
-    
+
     $containerBuilder->addDefinitions([
-        DepartmentDao::class => \DI\autowire(DepartmentDaoImpl::class),
-        EmployeeDao::class => \DI\autowire(EmployeeDaoImpl::class),
+        DepartmentDao::class => \DI\autowire(DepartmentDaoImpl2::class),
+        EmployeeDao::class => \DI\autowire(EmployeeDaoImpl2::class),
     ]);
 };

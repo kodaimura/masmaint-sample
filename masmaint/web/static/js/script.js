6408902a93ea8b3783e9a390451989223f80f92b
kodaimura <kodaimura123@gmail.com>
2024-11-09 17:14:10 +0900

    fix style

--- masmaint/web/static/js/script.js
+++ masmaint/web/static/js/script.js
@@ -1,4 +1,4 @@
-export const getJaTime = () => {
+const getJaTime = () => {
     const date = new Date();
     const utcOffset = date.getTimezoneOffset() * 60000;
     const jaTime = new Date(date.getTime() + utcOffset + 9 * 3600000);
